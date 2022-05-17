435e84d2873b97e3a4f4a805bc670776ae4e173b
moolnam <moolnam>
2022-05-17 15:18:33 +0900

    add

--- Chat-App-test/ConversationViewController.swift
+++ Chat-App-test/ConversationViewController.swift
@@ -10,9 +10,23 @@ import UIKit
 class ConversationViewController: UIViewController {
 
     override func viewDidLoad() {
+        print("view did load")
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        //asdasdd
+        view.backgroundColor = .white
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+        
+        if !isLoggedIn {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: true)
+        }
     }
 
 
